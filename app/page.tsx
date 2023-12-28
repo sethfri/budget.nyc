db6790534bbbfd3e2bba04a7b003eb617dd95dac
Seth Friedman <sethfri@users.noreply.github.com>
2023-12-28 00:19:43 -0500

    Add basic layout for main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,40 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import Search from './ui/search';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+    <main className="flex min-h-screen flex-col p-4 space-y-4 bg-slate-500">
+      <div className="flex bg-blue-300">
+        📊 Budget.nyc
       </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-        </div>
+      <div className="flex flex-col w-1/2 self-center space-y-4 bg-slate-300">
+        <h1 className="text-3xl">Where are my tax dollars going?</h1>
+        <p className="text-xl max-w-prose">Most New Yorkers pay X% of their income to the city. Input the city tax you paid in April 2023, and we'll show you where your money went.</p>
+        <Search placeholder={'City tax paid'} />
+        <table className="table-auto text-left w-full bg-yellow-300">
+          <thead>
+            <tr>
+              <th>Expense Category</th>
+              <th>Tax Dollars Spent</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Test</td>
+              <td>Test 2</td>
+            </tr>
+            <tr>
+              <td>Test</td>
+              <td>Test 2</td>
+            </tr>
+            <tr>
+              <td>Test</td>
+              <td>Test 2</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </main>
   );
