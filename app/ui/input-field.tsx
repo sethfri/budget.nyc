928d664ec92575b09d4d22bcda014724807b7d55
Seth Friedman <sethfri@users.noreply.github.com>
2023-12-29 03:40:26 -0500

    Fix input font size on mobile to prevent zooming

--- app/ui/input-field.tsx
+++ app/ui/input-field.tsx
@@ -10,7 +10,7 @@ export default function InputField({ placeholder, onChange }: { placeholder: str
         Search
       </label>
       <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-base md:text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={onChange}
       />
