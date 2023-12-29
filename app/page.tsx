fedcc4a420938f79d419a9f7f4d778640e61603b
Seth Friedman <sethfri@users.noreply.github.com>
2023-12-29 01:53:07 -0500

    Remove unused imports from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
 import { promises as fs } from 'fs';
 import Calculator from './ui/calculator';
 import { ExpenseRatio } from './lib/definitions';
